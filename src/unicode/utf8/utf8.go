e5dc030cc365f2b5a575a6804a3fa8c29f40aca9
Cauê Zaratin <caue.lasagno.zaratin@gmail.com>
2024-07-19 16:03:16 -0300

    feat: uft8 | started to create the utf8 package

---
+++ src/unicode/utf8/utf8.go
@@ -0,0 +1,132 @@
+// Package utf8 implements functions and constants to support text encoded in
+// UTF-8. It includes functions to translate between runes and UTF-8 byte sequences.
+// See https://en.wikipedia.org/wiki/UTF-8
+package utf8
+
+const (
+	// This rune and all above are represented as themself in UTF-8, which is 1 byte long. 
+	//For example, the ASCII character 'a' is 0x61 in UTF-8.
+	RuneSelf = 0x80
+)
+
+const (
+	t1 = 0b00000000
+	tx = 0b10000000
+	t2 = 0b11000000
+	t3 = 0b11100000
+	t4 = 0b11110000
+	t5 = 0b11111000
+
+	maskx = 0b00111111
+	mask2 = 0b00011111
+	mask3 = 0b00001111
+	mask4 = 0b00000111
+
+	rune1Max = 1<<7 - 1
+	rune2Max = 1<<11 - 1
+	rune3Max = 1<<16 - 1
+
+	// The default lowest and highest continuation byte.
+	locb = 0b10000000
+	hicb = 0b10111111
+
+	// These are the bytes that indicate the start of a UTF-8 sequence.
+	xx = 0xF1 // invalid: size 1
+	as = 0xF0 // ASCII: size 1
+	s1 = 0x02 // accept 0, size 2
+	s2 = 0x13 // accept 1, size 3
+	s3 = 0x03 // accept 0, size 3
+	s4 = 0x23 // accept 2, size 3
+	s5 = 0x34 // accept 3, size 4
+	s6 = 0x04 // accept 0, size 4
+	s7 = 0x44 // accept 4, size 4
+)
+
+// acceptRange gives the range of valid values for the second byte in a UTF-8
+// sequence.
+type acceptRange struct {
+	lo uint8 // lowest value for second byte.
+	hi uint8 // highest value for second byte.
+}
+
+// acceptRanges has size 16 to avoid bounds checks in the code that uses it.
+var acceptRanges = [16]acceptRange{
+	0: {locb, hicb},
+	1: {0xA0, hicb},
+	2: {locb, 0x9F},
+	3: {0x90, hicb},
+	4: {locb, 0x8F},
+}
+
+// first is information about the first byte in a UTF-8 sequence.
+// The information is represented as a 256-byte table, which is used to determine the length of the UTF-8 sequence.
+var first = [256]uint8{
+	//   1   2   3   4   5   6   7   8   9   A   B   C   D   E   F
+	as, as, as, as, as, as, as, as, as, as, as, as, as, as, as, as, // 0x00-0x0F
+	as, as, as, as, as, as, as, as, as, as, as, as, as, as, as, as, // 0x10-0x1F
+	as, as, as, as, as, as, as, as, as, as, as, as, as, as, as, as, // 0x20-0x2F
+	as, as, as, as, as, as, as, as, as, as, as, as, as, as, as, as, // 0x30-0x3F
+	as, as, as, as, as, as, as, as, as, as, as, as, as, as, as, as, // 0x40-0x4F
+	as, as, as, as, as, as, as, as, as, as, as, as, as, as, as, as, // 0x50-0x5F
+	as, as, as, as, as, as, as, as, as, as, as, as, as, as, as, as, // 0x60-0x6F
+	as, as, as, as, as, as, as, as, as, as, as, as, as, as, as, as, // 0x70-0x7F
+	//   1   2   3   4   5   6   7   8   9   A   B   C   D   E   F
+	xx, xx, xx, xx, xx, xx, xx, xx, xx, xx, xx, xx, xx, xx, xx, xx, // 0x80-0x8F
+	xx, xx, xx, xx, xx, xx, xx, xx, xx, xx, xx, xx, xx, xx, xx, xx, // 0x90-0x9F
+	xx, xx, xx, xx, xx, xx, xx, xx, xx, xx, xx, xx, xx, xx, xx, xx, // 0xA0-0xAF
+	xx, xx, xx, xx, xx, xx, xx, xx, xx, xx, xx, xx, xx, xx, xx, xx, // 0xB0-0xBF
+	xx, xx, s1, s1, s1, s1, s1, s1, s1, s1, s1, s1, s1, s1, s1, s1, // 0xC0-0xCF
+	s1, s1, s1, s1, s1, s1, s1, s1, s1, s1, s1, s1, s1, s1, s1, s1, // 0xD0-0xDF
+	s2, s3, s3, s3, s3, s3, s3, s3, s3, s3, s3, s3, s3, s4, s3, s3, // 0xE0-0xEF
+	s5, s6, s6, s6, s7, xx, xx, xx, xx, xx, xx, xx, xx, xx, xx, xx, // 0xF0-0xFF
+}
+
+func RuneCountInString(s string) (n int) {
+
+	ns := len(s)
+
+	for i := 0; i < ns; n++ {
+
+		c := s[i]
+
+		// Check if it is an ASCII character using the RuneSelf constant which is 0x80
+		// 0x80 is 128 in decimal, so if the character is less than 128, it is an ASCII character
+		if c < RuneSelf {
+			i++
+			continue
+		}
+
+		// Gets the first byte of the character
+		x := first[c]
+
+		// Checks if the character is invalid comparing it to the xx constant
+		// xx is 0xF1, which is 241 in decimal
+		if x == xx {
+			i++
+			continue
+		}
+
+		// gets the size of the character
+		size := int(x & 7)
+
+		// Checks if the character is incomplete
+		if i+size > ns {
+			i++
+			continue
+		}
+
+		accept := acceptRanges[x>>4] // Get the valid range for the next byte
+        if c := s[i+1]; c < accept.lo || accept.hi < c { // Validate the next byte
+            size = 1 // Adjust the size if invalid
+        } else if size == 2 {
+        } else if c := s[i+2]; c < locb || hicb < c { // Validate the third byte
+            size = 1 // Adjust the size if invalid
+        } else if size == 3 {
+        } else if c := s[i+3]; c < locb || hicb < c { // Validate the fourth byte
+            size = 1 // Adjust the size if invalid
+        }
+        i += size // Move to the next character
+	}
+
+	return 0
+}
